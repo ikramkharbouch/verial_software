1badc8dc279156fa50a0f82ce01a6eacffbbd637
ikramkharbouch <4573r14@gmail.com>
2024-12-03 12:28:25 +0100

    feat: implement multiple features and enhancements across the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'], // Update based on your project structure
   theme: {
-    extend: {},
+      extend: {},
   },
   plugins: [],
-}
+};
